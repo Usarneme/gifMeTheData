01a1878d02228b73ea9af7afacdd6e685f7d5157
Usarneme <jamestcoop@gmail.com>
2021-04-19 10:23:48 -0700

    start building out jquery event handler func
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,4 +5,17 @@ import './css/styles.css'
 
 // $("section").text("TEST TEXT INPUT")
 // console.log(process.env)
+$("form").submit((event) => {
+  event.preventDefault()
+})
 console.log($)
+
+// listen for keyword form submission
+// get the keyword
+// query the giphy api
+// validate the results
+// show the results
+
+
+
+// -> =>
