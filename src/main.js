1c4ac019d17a2d08a2c8fc7adf86c0d6e6595584
Usarneme <jamestcoop@gmail.com>
2021-04-20 11:11:21 -0700

    updates the apiQuery func to user promises instead of callback.
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- src/main.js
+++ src/main.js
@@ -12,27 +12,41 @@ $("form").submit((event) => {
 })
 
 function apiQuery(userSearch) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${process.env.API_KEY}&rating=pg-13`;
-  request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-      // call another func to deal with the response
-      // normally, we'd use the response here, but for testing, we can use the sample/mock data
-      getElements(response.data);
-    }
-  };
-  request.open("GET", url, true);
-  request.send();
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${process.env.API_KEY}&rating=pg-13`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(JSON.parse(request.responseText));
+      } else {
+        reject(JSON.parse(request.response));
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
+
+  promise.then(function(response) {
+    formatAndDisplay(response.data);
+  });
 }
 
-$("#faker").click(() => {
-  // console.log(mockDataResponse)
-  getElements(mockDataResponse.data)
-})
+// const apiQuery = (userSearch) => {
+//   let request = new XMLHttpRequest();
+//   const url = `http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${process.env.API_KEY}&rating=pg-13`;
+//   request.onreadystatechange = function() {
+//     if (this.readyState === 4 && this.status === 200) {
+//       const response = JSON.parse(this.responseText);
+//       console.log(response);
+//       // call another func to deal with the response
+//       formatAndDisplay(response.data);
+//     }
+//   };
+//   request.open("GET", url, true);
+//   request.send();
+// }
 
-function getElements(dataArray) {
+function formatAndDisplay(dataArray) {
   let html = `<div class="container p-1"><div class="row">`
   dataArray.forEach(element => {
     html += `<div class="card col-sm-3 text-center p-2 image-container" id="${element.id}"><img src="https://media2.giphy.com/media/${element.id}/giphy.gif" alt="${element.title}"><p class="text-center p-2">${element.title}</p></div>`
@@ -40,3 +54,9 @@ function getElements(dataArray) {
   html += `</div></div>`
   $("#gif-output").html(html).show()
 }
+
+$("#faker").click(() => {
+  // console.log(mockDataResponse)
+  formatAndDisplay(mockDataResponse.data)
+})
+
